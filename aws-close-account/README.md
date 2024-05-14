96b6a60610c1646a3f1c06b4d55a7b254c2f2045
tinhtq <truongquangtinh1997@gmail.com>
2024-05-14 13:49:33 +0700

    refactor

--- aws-close-account/README.md
+++
@@ -1,7 +0,0 @@
-## Close account in your organization
-
-Close your account in your organization after 90 days
-
-```bash
-aws organizations close-account --account-id <account-id>
-```
--- s3/public_accesss/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-    description = "Access key to AWS console"
-}
-variable "secret_key" {
-    description = "Secret key to AWS console"
-}
-variable "region" {
-    description = "AWS region"
-}
